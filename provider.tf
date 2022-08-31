94c13edfc485b4390f8cf238011f6bc741b00021
Tudor Datcu <tudor.nicolae.datcu@gmail.com>
2022-08-31 15:51:58 +0300

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+
+provider "oci" {
+  region = "eu-frankfurt-1"
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+}
