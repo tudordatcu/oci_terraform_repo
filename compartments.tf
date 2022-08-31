ebae614da809b17775d64fe2ac9726af4635272e
Tudor Datcu <tudor.nicolae.datcu@gmail.com>
2022-08-31 15:58:31 +0300

    Update compartments.tf

--- compartments.tf
+++ compartments.tf
@@ -1,10 +1,12 @@
+variable "compartment_ocid" {}
+
 locals {
     compartment_names = toset( ["test_compartment_1", "test_compartment_2"] )
 }
 
 resource "oci_identity_compartment" "tf-compartments" {
     for_each = local.compartment_names
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaacuepah577vse7ozgunlfulaznf5rvqaujutjgwbnc2ccmk53uk3a"
+    compartment_id = "${var.compartment_ocid}"
     description = "Test compartment for Terraform pipelines."
     enable_delete = true
     name = each.key
